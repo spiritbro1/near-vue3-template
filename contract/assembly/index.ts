49c8b36623d3c996f0507f1605a5b133a2075314
febriano <rinoakbr@gmail.com>
2022-05-08 10:46:32 +0700

    bismillah menang

---
+++ contract/assembly/index.ts
@@ -0,0 +1,21 @@
+
+import { u128,ContractPromise } from "near-sdk-as";
+
+export function grant_minter(account_id:string):void{
+
+
+  let nftArgs:Minter = {account_id};
+  let promise = ContractPromise.create(
+    "panda.mintspace2.testnet", // contract account name
+    "grant_minter", // contract method name
+    nftArgs.encode(), // serialized contract method arguments encoded as Uint8Array
+    20e12,
+    u128.fromString("1")
+  );
+  promise.returnAsResult()
+ 
+}
+@nearBindgen
+class Minter{
+  account_id:string;
+}
